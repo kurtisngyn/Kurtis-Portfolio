e61f62eb3a096a93d7ff86a956cd3b5a36977f19
kychhu <kurtisnguyen1234@gmail.com>
2024-12-01 14:36:59 -0800

    create header for craiglistpage

---
+++ Kurtis_Portfolio/src/components/Craiglist-header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function CraiglistHeader() {
+    return (
+        <div>
+            <h1>Craiglist Redesign</h1>
+        </div>
+    );
+}
+
+export default CraiglistHeader;
--- Kurtis_Portfolio/src/pages/artifacts/craigslist.jsx
+++ Kurtis_Portfolio/src/pages/artifacts/craigslist.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../../components/Header";
 import BackBtn from "../../components/Back-btn";
 import Craigslistphoto from "../../assets/craigslistphoto.png";
+import CraiglistHeader from "../../components/Craiglist-header";
 
 function Craigslist() {
     return (
         <div className="flex flex-col bg-lavendar">
             <Header /> 
             <BackBtn />
+            <div className="flex-grow flex items-center justify-center overflow-hidden px-4 lg:px-6 font-quicksand font-semibold text-6xl mb-6">
+            <CraiglistHeader />
+            </div>
             <div className="flex-grow flex items-center justify-center overflow-hidden px-4 lg:px-6 ">
                 <img src={Craigslistphoto} alt="Craigslistintrophoto" className="mt-4 mx-auto" />
             </div>
