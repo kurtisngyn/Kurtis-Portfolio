ea3d063aabecc6f43d96e8bf7b94a40816a94201
kychhu <kurtisnguyen1234@gmail.com>
2024-12-09 20:12:42 -0800

    Update

--- Kurtis_Portfolio/src/pages/About.jsx
+++ Kurtis_Portfolio/src/pages/About.jsx
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 import Header from "../components/Header";
 import AbtSummary from "../components/Abtmepieces/Abtsummary";
 import ImageCarousel from "../components/Abtmepieces/ImageCarousel";
-import PersonalCard from "../components/Abtmepieces/PersonalCard";
+import PersonalCard from "../components/Abtmepieces/Personalcard";
 import ToolSection from "../components/Abtmepieces/ToolSection";
 
 const About = () => {
