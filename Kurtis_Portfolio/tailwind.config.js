48eaa3ad235043fbab7b5fef5b3b94dea596e082
kychhu <kurtisnguyen1234@gmail.com>
2025-01-16 14:41:42 -0800

    Update tailwind.config.js
    Add responsive screen sizes

--- Kurtis_Portfolio/tailwind.config.js
+++ Kurtis_Portfolio/tailwind.config.js
@@ -15,6 +15,23 @@ export default {
         'black': '#333333',
         'white': '#F5F5F5',
       },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+  
       fontFamily: {
         quicksand: ['Quicksand', 'sans-serif'],
       },
