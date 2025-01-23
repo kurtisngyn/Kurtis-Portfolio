3ab4b39046d1ec5b0f5119fab54c9bb80a9fb325
kychhu <kurtisnguyen1234@gmail.com>
2025-01-23 14:40:58 -0800

    Update Header.jsx

--- Kurtis_Portfolio/src/components/Header.jsx
+++ Kurtis_Portfolio/src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link component from react-router-dom
+import { Link } from "react-router-dom";
 import Logo from "../assets/Logo.svg";
 
 const Header = () => {
-
     const emailAddress = "kurtisnguyen1234@gmail.com";
   
     const handleEmailClick = () => {
@@ -11,17 +10,21 @@ const Header = () => {
     };
 
   return (
-    <header className="flex items-center justify-between px-12 py-2 bg-lavender font-quicksand text-white text-lg">
-      {/* Styling for icon*/}
+    <header className="flex items-center justify-between px-4 sm:px-6 lg:px-8 py-2 bg-lavender font-quicksand text-white text-lg">
       <div className="flex items-center space-x-2">
         <Link to="/">
-          <img src={Logo} alt="Logo" className="w-32 h-32 cursor-pointer" />
+          <img 
+            src={Logo} 
+            alt="Logo" 
+            className="w-32 h-32 cursor-pointer" 
+          />
         </Link>
-        {/* <span className="font-semibold text-lg">Designs</span> */}
       </div>
 
-      {/* Button on the right */}
-      <button onClick= {handleEmailClick} className="px-6 py-3 bg-black hover:bg-gray-700 text-white font-quicksand font-bold rounded-lg shadow-lg w-[130px]">
+      <button 
+        onClick={handleEmailClick} 
+        className="px-6 py-3 bg-black hover:bg-gray-700 text-white font-quicksand font-bold rounded-lg shadow-lg w-[130px]"
+      >
         Email me
       </button>
     </header>
