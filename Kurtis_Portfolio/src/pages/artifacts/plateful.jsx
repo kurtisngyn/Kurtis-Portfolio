8db9182a56f20ce9ac4211fa01fef14a86b26311
kychhu <kurtisnguyen1234@gmail.com>
2025-03-03 10:47:21 -0800

    Fix broken project page
    Add work in progress to existing page that didn't have content

--- Kurtis_Portfolio/src/pages/artifacts/plateful.jsx
+++ Kurtis_Portfolio/src/pages/artifacts/plateful.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import TBA from "../../components/TBA";
 
 function Plateful() {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen bg-lavender">
             <Header />
+            <TBA />
             <Footer />
         </div>
+        
     );
 }
 
